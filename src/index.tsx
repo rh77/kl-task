0527b8c41d78999a991648d267b8acad38e73289
rh77 <rhawaiian@gmail.com>
2019-01-20 02:21:59 +0300

    added autoprefixer, sass, tslint, polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
