169aa79860f233036819489386d58b77d84fdf84
rh77 <rhawaiian@gmail.com>
2019-01-22 18:32:20 +0300

    added navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,16 @@ import Route from './blocks/Routing/Route';
 import Router from './blocks/Routing/Router';
 import Users from './blocks/Users';
 import Welcome from './blocks/Welcome';
-import logo from './logo.svg';
 
 class App extends Component {
   public render(): JSX.Element {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app__header">
+          <div className="app__link-container">
+            <a className="app__link" href="#welcome">Welcome</a>
+            <a className="app__link" href="#users">Users list</a>
+          </div>
         </header>
         <Router>
           <Route path="welcome" showWhenEmpty={true} block={Welcome}/>
