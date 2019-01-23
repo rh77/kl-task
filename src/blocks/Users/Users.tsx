abb1a851c89c9b91be4d1903805cb1548c3e3298
rh77 <rhawaiian@gmail.com>
2019-01-23 20:10:44 +0300

    added tile and group views

--- src/blocks/Users/Users.tsx
+++ src/blocks/Users/Users.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Users.scss';
 
-export enum ViewType { Default=0, Group=1, Tile=2, Table=3 };
+export enum ViewType { Default= 0, Group= 1, Tile= 2, Table= 3 }
 
 export interface IUsersProps {
   viewType: ViewType;
@@ -17,20 +17,17 @@ class Users extends Component<IUsersProps, IUsersState> {
 
     this.state = {
       viewType: ViewType.Tile
-    }
+    };
   }
 
   public render(): JSX.Element {
-    const data = [1, 2, 3, 4, 5, 6, 7];
     return (
-      <div className="users-header">
-        <div className="users-header__panel">
-          <div className="users-header__search"><Search/></div>
-          <div className="users-header__layout-switcher"><Switcher viewType={this.state.viewType} onClick={this.handleClick}/></div>
+      <div className="users-container">
+        <div className="users-container__header-panel">
+          <Search/>
+          <Switcher viewType={this.state.viewType} onClick={this.handleClick}/>
         </div>
-        <div className="users-table">
-        {data.map((val: number) => <Line key={val} valueObject={val} isUnmanaged={val === 5 || val === 6}/>)}
-        </div>
+        <UsersLayout viewType={this.state.viewType}/>
       </div>
     );
   }
@@ -41,6 +38,50 @@ class Users extends Component<IUsersProps, IUsersState> {
     });
   }
 }
+
+const UsersLayout = (props: { viewType: ViewType }): JSX.Element => {
+
+  const data: number[] = [];
+  for (let i = 1; i < 9; i++) {
+    data.push(i);
+  }
+
+  switch (props.viewType) {
+
+    case ViewType.Table:
+      return (
+        <div className="users-container__content-panel users-table">
+          {data.map((val: number) => <Line key={val} valueObject={val} isUnmanaged={val === 5 || val === 6}/>)}
+        </div>);
+
+    case ViewType.Group:
+        return (
+          <div className="users-container__content-panel">
+            <ul className="user-groups">
+              {data.map((val: number) => <Group key={val} valueObject={val}/>)}
+            </ul>
+          </div>);
+
+    case ViewType.Tile:
+    default:
+      const placeHolders = new Array(10).fill(0);
+      return (
+            <div className="users-container__content-panel">
+              <ul className="user-tiles">
+                {data.map((val: number) => <Tile key={val} valueObject={val}/>)
+                .concat(placeHolders.map((_, i) => <li key={i} className="user-tiles__placeholder"/>))}
+              </ul>
+            </div>);
+  }
+};
+
+const Group = (props: { valueObject: number }): JSX.Element => {
+  return <li className="user-groups__group">{props.valueObject}</li>;
+};
+
+const Tile = (props: { valueObject: number }): JSX.Element => {
+  return <li className="user-tiles__tile">{props.valueObject}</li>;
+};
 
 const Line = (props: { valueObject: number, isUnmanaged: boolean}): JSX.Element => {
   const classes = 
@@ -61,9 +102,18 @@ const Search = (): JSX.Element => {
 const Switcher = (props: { viewType: ViewType, onClick: any }): JSX.Element => {
   return (
     <div className="switcher">
-      <div className={getClassName("group", props.viewType === ViewType.Group )} onClick={() => props.onClick(ViewType.Group)}/>
-      <div className={getClassName("tile", props.viewType === ViewType.Tile )} onClick={() => props.onClick(ViewType.Tile)}/>
-      <div className={getClassName("table", props.viewType === ViewType.Table )} onClick={() => props.onClick(ViewType.Table)}/>
+      <div 
+        className={getClassName("group", props.viewType === ViewType.Group)} 
+        onClick={() => props.onClick(ViewType.Group)}
+      />
+      <div 
+        className={getClassName("tile", props.viewType === ViewType.Tile)} 
+        onClick={() => props.onClick(ViewType.Tile)}
+      />
+      <div 
+        className={getClassName("table", props.viewType === ViewType.Table)} 
+        onClick={() => props.onClick(ViewType.Table)}
+      />
     </div>);
 
   function getClassName(type: string, isActive: boolean): string {
