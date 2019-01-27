296a9e3f5479b249b0c02bf2d61d7daf28edcaea
rh77 <rhawaiian@gmail.com>
2019-01-28 01:53:17 +0300

    fixed checkbox on tile

--- src/blocks/Users/Layouts/Tiles/Tile.tsx
+++ src/blocks/Users/Layouts/Tiles/Tile.tsx
@@ -3,13 +3,14 @@ import UserModel from '../../../../model/userModel';
 import placeHolderImage from "./img/avatar-placeholder.png";
 import "./Tiles.scss";
 
-export default class Tile extends Component<{ userModel: UserModel }, { loaded: boolean }> {
+export default class Tile extends Component<{ userModel: UserModel }, { loaded: boolean, checked: boolean }> {
     private image: HTMLImageElement;
 
     constructor(props: { userModel: UserModel }) {
         super(props);
 
-        this.state = {
+        this.state = {          
+            checked: false,
             loaded: false
         };
 
@@ -17,7 +18,9 @@ export default class Tile extends Component<{ userModel: UserModel }, { loaded: 
         this.image.onload = () => {
             this.setState({ loaded: true });
         };
+
         this.image.src = this.props.userModel.picture;
+        this.onCheckedChanged = this.onCheckedChanged.bind(this);
     }
 
     public render() {
@@ -26,12 +29,27 @@ export default class Tile extends Component<{ userModel: UserModel }, { loaded: 
         const groupClassName = "user-tile__group" + (groupId === 0 ? " user-tile__group_unmanaged" : "");
         return (
             <li className="user-tiles__tile user-tile">
-                <input className="user-tile__check" type="checkbox"/>
+                <div className="user-tile__check-plate" onClick={this.onCheckedChanged}>
+                    <input 
+                        className="user-tile__check" 
+                        type="checkbox" 
+                        checked={this.state.checked} 
+                        onChange={this.onCheckedChanged}
+                    />
+                </div>
                 <button className="user-tile__upload-button"/>
                 <label className="user-tile__name">{name}</label>
                 <img className="user-tile__picture" src={pictureSrc}/>
                 <label className={groupClassName}>{group}</label>
                 <label className="user-tile__phone">{phone}</label>
             </li>);
+    }
+
+    private onCheckedChanged(e: React.FormEvent<HTMLDivElement>): boolean {
+        this.setState({
+            checked: !this.state.checked,
+            loaded: this.state.loaded
+        });
+        return false;
     }
 }
