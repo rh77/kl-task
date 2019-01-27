3b0ceda060269df3d056b9596ad4e45ab6bd56e5
rh77 <rhawaiian@gmail.com>
2019-01-28 01:54:00 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kl-task/precache-manifest.80af09a76024117f25fbd2689cbe2185.js"
+  "/kl-task/precache-manifest.135289bcf5d0ac69d9be75770b054c38.js"
 );
 
 workbox.clientsClaim();
