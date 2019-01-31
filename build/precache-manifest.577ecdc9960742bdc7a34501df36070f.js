06bbc15d9510f4efcd97737a959052710218abff
rh77 <rhawaiian@gmail.com>
2019-01-31 03:10:38 +0300

    add build folder

---
+++ build/precache-manifest.577ecdc9960742bdc7a34501df36070f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0e4a0b34f48e9c5249b255f1f930090",
+    "url": "/kl-task/static/media/tile-view.c0e4a0b3.svg"
+  },
+  {
+    "revision": "54f8aa479c5e69148c2f3bfba33d2476",
+    "url": "/kl-task/static/media/table-view.54f8aa47.svg"
+  },
+  {
+    "revision": "f49be5ebf3a143d8e653fe39f76f364e",
+    "url": "/kl-task/static/media/group-view.f49be5eb.svg"
+  },
+  {
+    "revision": "da552689c0a15f7edb7779cda27188f1",
+    "url": "/kl-task/static/media/cloud-upload.da552689.svg"
+  },
+  {
+    "revision": "9dc895c06c55663ebf5a",
+    "url": "/kl-task/static/js/runtime~main.9dc895c0.js"
+  },
+  {
+    "revision": "7d6342be04df74f39772",
+    "url": "/kl-task/static/js/main.7d6342be.chunk.js"
+  },
+  {
+    "revision": "65c0cdaf862c1988a133",
+    "url": "/kl-task/static/js/1.65c0cdaf.chunk.js"
+  },
+  {
+    "revision": "7d6342be04df74f39772",
+    "url": "/kl-task/static/css/main.d52345fb.chunk.css"
+  },
+  {
+    "revision": "1662381c9be0d6ca943ad8c0f62acb8c",
+    "url": "/kl-task/index.html"
+  }
+];
