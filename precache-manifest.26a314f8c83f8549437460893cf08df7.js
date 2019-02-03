7a8a43e0fc5496416d85befa27b0cc1a8e74cc20
rh77 <rhawaiian@gmail.com>
2019-02-03 21:44:35 +0300

    Updates

--- precache-manifest.26a314f8c83f8549437460893cf08df7.js
+++ precache-manifest.26a314f8c83f8549437460893cf08df7.js
@@ -20,19 +20,19 @@ self.__precacheManifest = [
     "url": "/kl-task/static/js/runtime~main.9dc895c0.js"
   },
   {
-    "revision": "ad027703e2155f5d957c",
-    "url": "/kl-task/static/js/main.ad027703.chunk.js"
+    "revision": "fd3caa212c0a1680e9f1",
+    "url": "/kl-task/static/js/main.fd3caa21.chunk.js"
   },
   {
     "revision": "973179166a69301f9612",
     "url": "/kl-task/static/js/1.97317916.chunk.js"
   },
   {
-    "revision": "ad027703e2155f5d957c",
-    "url": "/kl-task/static/css/main.6aec094a.chunk.css"
+    "revision": "fd3caa212c0a1680e9f1",
+    "url": "/kl-task/static/css/main.fb197cc3.chunk.css"
   },
   {
-    "revision": "a5787b67386b2599e76e048e8b00ea2a",
+    "revision": "6ea930c91fb294095f85da999a6b4efb",
     "url": "/kl-task/index.html"
   }
 ];
